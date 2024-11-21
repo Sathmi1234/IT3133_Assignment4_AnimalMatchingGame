62df58051e99ba562429aed37b0d4360f3f6e077
Sathmi1234 <devsadini@gmail.com>
2024-11-21 19:19:25 +0530

    displaying name sent from the parent

--- assignment4/src/components/Name.js
+++ assignment4/src/components/Name.js
@@ -1,8 +1,8 @@
-export default function Name(){
+export default function Name({name}){
     return(
         <div>
             <h2>Animal Name</h2>
+            <h2>{name}</h2>
         </div>
     )
-    
 }
